d1a1af2bb70d0b4d9828590da6958cdc905c782a
Nolan Kohler <kohlern@wlhs.wlwv.k12.or.us>
2019-10-03 09:54:20 -0700

    Boolean Practice

--- DrinkDrive.java
+++ DrinkDrive.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 public class DrinkingAge{
 public static void main(String [] args){
+
   Scanner scan = new Scanner(System.in);
   int age = scan.nextInt();
+
   if(age >= 21)
-    System.out.println("Of Age");
+    System.out.println("Can Drink and Drive")
+    else if(age >=17 && age<=21)
+    System.out.println("Can Drive but Not Drink");
     else
-      System.out.println("Denied");
+    System.out.println("Can't Drive or Drive");
+
+
+
 }
-}
+  }
