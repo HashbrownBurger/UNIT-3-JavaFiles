0c8e982e46ca05ca0f5ceb8bda57ad8d7a8e1c68
Nolan Kohler <kohlern@wlhs.wlwv.k12.or.us>
2019-10-10 09:56:15 -0700

    Palindrome

---
+++ PalindromeTester.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class WinPercent{
+  public static void main(String[]args){
+    System.out.println("Would You Like to Enter a Palindrome (y//n)");
+    Scanner scan = new Scanner(System.in);
+    int control = 0;
+    String answer = scan.next(System.in);
+
+      if(answer.equals(n)){
+        control++;
+      }
+
+      System.out.println(control);
+
+}
+}
